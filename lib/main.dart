ac305adace1245169492c9ed9456dd3d5af0f90b
fadihere <fadihere@outlook.com>
2021-12-27 12:27:40 +0500

    Add functions in Both Dicee

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, prefer_typing_uninitialized_variables, must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -23,34 +24,43 @@ void main() {
   );
 }
 
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
 class _DicePageState extends State<DicePage> {
   @override
+  int left = 1;
+  int right = 2;
+
+  void random() {
+    setState(() {
+      left = Random().nextInt(6) + 1;
+      right = Random().nextInt(6) + 1;
+    });
+  }
+
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(0.0),
-              child: IconButton(
-                iconSize: 200,
-                onPressed: () {
-                  setState()
-                  },                },
-                icon: Image.asset('images/dice$left.png'),
-              ),
+            child: TextButton(
+              onPressed: () {
+                random();
+              },
+              child: Image.asset('images/dice$left.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(0.0),
-              child: IconButton(
-                iconSize: 200,
-                onPressed: () {
-                  print('Right Button Pressed');
-                },
-                icon: Image.asset('images/dice1.png'),
-              ),
+            child: TextButton(
+              onPressed: () {
+                random();
+              },
+              child: Image.asset('images/dice$right.png'),
             ),
           ),
         ],
